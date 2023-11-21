c9b15313549f6cd0176e2242a28f5f61e7a37afd
freelancernishad <freelancernishad123@gmail.com>
2023-11-21 22:07:36 +0600

    upload

---
+++ app/Models/student.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class student extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+
+        'school_id',
+        'Year',
+        'AdmissionID',
+        'StudentID',
+        'StudentRoll',
+        'section',
+        'sscroll',
+        'StudentClass',
+        'StudentGender',
+        'StudentReligion',
+        'StudentName',
+        'StudentNameEn',
+        'StudentFatherNameBn',
+        'StudentFatherName',
+        'StudentMotherNameBn',
+        'StudentMotherName',
+        'StudentFatherNid',
+        'StudentMotherNid',
+        'StudentFatherBCN',
+        'StudentMotherBCN',
+        'guardNameBn',
+        'guardName',
+        'guardNid',
+        'guardRalation',
+        'StudentFatherOccupation',
+        'parentEarn',
+        'StudentMotherOccupation',
+        'ParentEmail',
+        'ParentPassword',
+        'StudentEmail',
+        'StudentPassword',
+        'StudentDateOfBirth',
+        'StudentBirthCertificateNo',
+        'StudentCategory',
+        'StudentKota',
+        'StudentKotaSonodNo',
+        'preSchool',
+        'preClass',
+        'bigBroSis',
+        'bigBroSisName',
+        'bigBroSisClass',
+        'bigBroSisGroup',
+        'bigBroSisRoll',
+        'StudentGroup',
+        'StudentSubject',
+        'StudentAddress',
+        'division',
+        'district',
+        'upazila',
+        'union',
+        'post_office',
+        'StudentPhoneNumber',
+        'AreaPostalCode',
+        'stipend',
+        'StudentStatus',
+        'StudentTranferFrom',
+        'StudentPicture',
+        'JoiningDate',
+        'StudentTranferStatus',
+        'AplicationStatus',
+        'ThisMonthPaymentStatus',
+
+
+
+    ];
+
+
+    public function Payments(){
+        return $this->belongsTo(payment::class, 'StudentID', 'studentId');
+    }
+}
